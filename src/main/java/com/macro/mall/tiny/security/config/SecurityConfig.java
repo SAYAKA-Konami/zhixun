b137ec8e36429b78963b04d9287ec2dd98d3e3a7
mangosteen <katohuge@outlook.com>
2024-03-09 22:29:13 +0800

    重写security配置类

--- src/main/java/com/macro/mall/tiny/security/config/SecurityConfig.java
+++ src/main/java/com/macro/mall/tiny/security/config/SecurityConfig.java
@@ -1,16 +1,18 @@
 package com.macro.mall.tiny.security.config;
 
-import com.macro.mall.tiny.security.component.*;
+import com.macro.mall.tiny.security.component.DynamicAuthorizationManager;
+import com.macro.mall.tiny.security.component.JwtAuthenticationTokenFilter;
+import com.macro.mall.tiny.security.component.RestAuthenticationEntryPoint;
+import com.macro.mall.tiny.security.component.RestfulAccessDeniedHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
@@ -36,34 +38,53 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry registry = httpSecurity
-                .authorizeHttpRequests();
-        //不需要保护的资源路径允许访问
-        for (String url : ignoreUrlsConfig.getUrls()) {
-            registry.requestMatchers(url).permitAll();
-        }
-        //允许跨域请求的OPTIONS请求
-        registry.requestMatchers(HttpMethod.OPTIONS)
-                .permitAll();
-        // 任何请求需要身份认证
-        registry.and()
-                .authorizeHttpRequests()
-                .anyRequest()
-                .access(dynamicAuthorizationManager)
-                // 关闭跨站请求防护及不使用session
-                .and()
-                .csrf()
-                .disable()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                // 自定义权限拒绝处理类
-                .and()
-                .exceptionHandling()
-                .accessDeniedHandler(restfulAccessDeniedHandler)
-                .authenticationEntryPoint(restAuthenticationEntryPoint)
-                // 自定义权限拦截器JWT过滤器
-                .and()
-                .addFilterBefore(jwtAuthenticationTokenFilter, UsernamePasswordAuthenticationFilter.class);
-        return httpSecurity.build();
+        HttpSecurity security = httpSecurity.authorizeHttpRequests(registry -> {
+            ignoreUrlsConfig.getUrls().forEach(u -> registry.requestMatchers(u).permitAll());
+            registry.requestMatchers(HttpMethod.OPTIONS).permitAll()
+                    // 任何请求需要身份认证
+                    .anyRequest()
+                    .access(dynamicAuthorizationManager);
+        });
+        // 关闭跨站请求防护
+        security.csrf(AbstractHttpConfigurer::disable);
+        // 不使用session
+        security.sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        // 自定义权限拒绝处理类
+        security.exceptionHandling(s -> {
+            s.accessDeniedHandler(restfulAccessDeniedHandler)
+                    .authenticationEntryPoint(restAuthenticationEntryPoint);
+        });
+        // 自定义权限拦截器JWT过滤器
+        security.addFilterBefore(jwtAuthenticationTokenFilter, UsernamePasswordAuthenticationFilter.class);
+        return security.build();
+//        AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry registry = httpSecurity
+//                .authorizeHttpRequests();
+//        //不需要保护的资源路径允许访问
+//        for (String url : ignoreUrlsConfig.getUrls()) {
+//            registry.requestMatchers(url).permitAll();
+//        }
+//        //允许跨域请求的OPTIONS请求
+//        registry.requestMatchers(HttpMethod.OPTIONS)
+//                .permitAll();
+//        // 任何请求需要身份认证
+//        registry.and()
+//                .authorizeHttpRequests()
+//                .anyRequest()
+//                .access(dynamicAuthorizationManager)
+//                // 关闭跨站请求防护及不使用session
+//                .and()
+//                .csrf()
+//                .disable()
+//                .sessionManagement()
+//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                // 自定义权限拒绝处理类
+//                .and()
+//                .exceptionHandling()
+//                .accessDeniedHandler(restfulAccessDeniedHandler)
+//                .authenticationEntryPoint(restAuthenticationEntryPoint)
+//                // 自定义权限拦截器JWT过滤器
+//                .and()
+//                .addFilterBefore(jwtAuthenticationTokenFilter, UsernamePasswordAuthenticationFilter.class);
+//        return httpSecurity.build();
     }
 }
