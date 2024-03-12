20ff9d8794c96ea5681c7a2fdc75e98d7cefc740
mangosteen <katohuge@outlook.com>
2024-03-13 02:26:10 +0800

    移除无用的注释

--- src/main/java/com/macro/mall/tiny/modules/task/service/CustomerService.java
+++ src/main/java/com/macro/mall/tiny/modules/task/service/CustomerService.java
@@ -3,14 +3,7 @@ package com.macro.mall.tiny.modules.task.service;
 import com.macro.mall.tiny.modules.task.model.Customer;
 import com.baomidou.mybatisplus.extension.service.IService;
 
-/**
- * <p>
- *  服务类
- * </p>
- *
- * @author macro
- * @since 2024-03-13
- */
+
 public interface CustomerService extends IService<Customer> {
 
 }
--- src/main/java/com/macro/mall/tiny/modules/task/service/TaskCustomerService.java
+++ src/main/java/com/macro/mall/tiny/modules/task/service/TaskCustomerService.java
@@ -3,14 +3,6 @@ package com.macro.mall.tiny.modules.task.service;
 import com.macro.mall.tiny.modules.task.model.TaskCustomer;
 import com.baomidou.mybatisplus.extension.service.IService;
 
-/**
- * <p>
- *  服务类
- * </p>
- *
- * @author macro
- * @since 2024-03-13
- */
 public interface TaskCustomerService extends IService<TaskCustomer> {
 
 }
